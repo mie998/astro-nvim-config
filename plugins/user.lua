026f8dfae75a6de262480a38a1920ce1a6a00cd3
mie998 <nishiwaki.kyoto@gmail.com>
2023-07-09 14:49:31 +0900

    update copilot setting

--- plugins/user.lua
+++ plugins/user.lua
@@ -26,15 +26,12 @@ return {
         },
         suggestion = {
           enabled = true,
-          auto_trigger = false,
-          debounce = 75,
+          auto_trigger = true,
           keymap = {
             accept = "<M-l>",
-            accept_word = false,
-            accept_line = false,
             next = "<M-]>",
             prev = "<M-[>",
-            dismiss = "<C-]>",
+            dismiss = "<M-,>",
           },
         },
         filetypes = {
